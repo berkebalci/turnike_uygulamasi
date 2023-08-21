e0f7f437c20e7e65002c069bb2e6c8818f4c62ac
berkebalci <berkebalcii38@gmail.com>
2023-08-21 13:55:12 +0300

    Global value has added

--- lib/Global/global.dart
+++ lib/Global/global.dart
@@ -1,3 +1,4 @@
 import 'package:rxdart/rxdart.dart';
 
-BehaviorSubject<String> colorValue = BehaviorSubject<String>.seeded("white");
+BehaviorSubject<String> colorValue = BehaviorSubject<String>(); //For theme color
+BehaviorSubject<bool> textfieldVisibility = BehaviorSubject<bool>.seeded(false); //For animation
--- lib/view/login_page.dart
+++ lib/view/login_page.dart
@@ -37,7 +37,7 @@ class _LoginPageState extends State<LoginPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(
+      title: Text(
           "Login Page",
           style: GoogleFonts.roboto(),
         ),
