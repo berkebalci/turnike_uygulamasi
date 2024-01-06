17ec05c97227f4691842e249a1403f7385205f81
Berke Balci <74725559+berkebalci@users.noreply.github.com>
2024-01-06 13:08:39 +0300

    Update card_service.dart

--- lib/service/methods/card_service.dart
+++ lib/service/methods/card_service.dart
@@ -8,23 +8,14 @@ import 'package:http/http.dart' as http;
 class CardService {
   static Future<Response> cardRequest(
       String loginToken, String cardId, int? hotelId) async {
-    final url =
-        "https://4001.hoteladvisor.net/Execute/SP_PARK_TURNSTILE_PASS_CONTROL_BY_GUESTID";
+    //endpoint private
 
     try {
       final response = await http.post(
         Uri.parse(url),
         headers: {'content-type': 'application/json'},
         body: json.encode({
-          "Action": "Execute",
-          "Object": "SP_PARK_TURNSTILE_PASS_CONTROL_BY_GUESTID",
-          "LoginToken": loginToken,
-          "Parameters": {
-            "TURNSTILEID": 18,
-            "CARDID": cardId,
-            "HOTELID": hotelId,
-            "DIRECTION": 1,
-            "SELECTTT": 0,
+         //parameters private
           },
         }),
       );
